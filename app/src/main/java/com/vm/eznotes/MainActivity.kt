6f4e8ddf714e10ae77db82febe627877169841ce
Marcell Varszegi <varszegimarcell@gmail.com>
2020-01-11 14:50:30 +0100

    Started application development.

---
+++ app/src/main/java/com/vm/eznotes/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vm.eznotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class aMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
