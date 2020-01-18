0697ba3fb9deb0b3ddaacd051aa96465a794b347
Marcell Varszegi <varszegimarcell@gmail.com>
2020-01-18 17:09:10 +0100

    Added new method.

--- app/src/main/java/com/vm/eznotes/Notes.kt
+++ app/src/main/java/com/vm/eznotes/Notes.kt
@@ -3,5 +3,12 @@ package com.vm.eznotes
 class Notes {
     companion object{
         var Items = mutableListOf<Note>()
+        fun isNameAlreadyInUse(str : String) : Boolean {
+            val iterator = Items.iterator()
+            iterator.forEach { note ->
+                if(note.Name == str) return true
+            }
+            return false
+        }
     }
 }
